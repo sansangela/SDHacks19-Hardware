6467031fcee3c14b2df61aca99709640c4017115
sansangela <sansangela@hotmail.com>
2019-10-26 15:48:27 -0700

    added a digital clock and an icon

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
 console.log('Hello world!');
+
+import clock from "clock";
+import document from "document";
+// Import the messaging module
+import * as messaging from "messaging";
+
+// CLOCK
+let myClock = document.getElementById("myClock");
+
+clock.granularity = 'seconds'; // seconds, minutes, hours
+
+clock.ontick = function(evt) {
+  myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+                      ("0" + evt.date.getMinutes()).slice(-2) + ":" +
+                      ("0" + evt.date.getSeconds()).slice(-2);
+};
+
+// MESSAGEÍ
+// Listen for the onmessage event
+messaging.peerSocket.onmessage = function(evt) {
+  // Output the message to the console
+  console.log(JSON.stringify(evt.data));
+}
