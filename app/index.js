fa8edb4b745b7b7b7de527534a18a00ae33a7792
jingxiang4766 <43655970+jingxiang4766@users.noreply.github.com>
2019-10-26 21:58:14 -0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,54 +5,6 @@ import document from "document";
 import * as messaging from "messaging";
 import { geolocation } from "geolocation";
 
-// CLOCK
-// Update the clock every second
-clock.granularity = "seconds";
-
-let hourHand = document.getElementById("hours");
-let minHand = document.getElementById("mins");
-let secHand = document.getElementById("secs");
-
-// Returns an angle (0-360) for the current hour in the day, including minutes
-function hoursToAngle(hours, minutes) {
-  let hourAngle = (360 / 12) * hours;
-  let minAngle = (360 / 12 / 60) * minutes;
-  return hourAngle + minAngle;
-}
-
-// Returns an angle (0-360) for minutes
-function minutesToAngle(minutes) {
-  return (360 / 60) * minutes;
-}
-
-// Returns an angle (0-360) for seconds
-function secondsToAngle(seconds) {
-  return (360 / 60) * seconds;
-}
-
-// Rotate the hands every tick
-function updateClock() {
-  let today = new Date();
-  let hours = today.getHours() % 12;
-  let mins = today.getMinutes();
-  let secs = today.getSeconds();
-
-  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
-  minHand.groupTransform.rotate.angle = minutesToAngle(mins);
-  secHand.groupTransform.rotate.angle = secondsToAngle(secs);
-}
-
-// Update the clock every tick event
-clock.ontick = () => updateClock();
-
-
-// MESSAGE
-// Listen for the onmessage event
-messaging.peerSocket.onmessage = function(evt) {
-  // Output the message to the console
-  console.log(JSON.stringify(evt.data));
-}
-
 
 // GEOLOCATION
 // geolocation.getCurrentPosition(locationSuccess, locationError);
@@ -90,4 +42,18 @@ function locationSuccess(position) {
 function locationError(error) {
   console.log("Error: " + error.code,
               "Message: " + error.message);
+}
+
+
+//button
+import document from "document";
+
+let counter = 1;
+let mybutton = document.getElementById("mybutton");
+if (mybutton){
+  console.log("buttom exist");
+  mybutton.onactivate = function(evt) {
+    console.log("clicked " +counter+ " times");
+    counter++;
+  }
 }
